82ff5150a403d9b7c43bcfa31d43efe0f64f2f5b
Thomas Ferreira <thomas_ferreira@yahoo.fr>
2016-12-23 15:05:03 +0100

    Update config file

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # Config file, please set the following settings:
 
 # host the bridge is listening for, default: localhost
-host = localhost
+host = "0.0.0.0"
 
 # listening port, default 5000
 port = 5454
 
 # url to post bridged webhooks to, default
 # http://youplatformserver.com/hooks/
-webhook_url = https://chat.orbb.fr/hooks/ymk7br3xzfr3zbc3e5pb7gc38o
+webhook_url = "https://chat.orbb.fr/hooks/"
